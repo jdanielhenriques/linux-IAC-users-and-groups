ae5b972cfa8acf7dfb25f9a583748981bf0cf1b0
jdanielhenriques <62913703+jdanielhenriques@users.noreply.github.com>
2022-07-31 10:32:15 +0100

    Add script-users-groups.sh file

---
+++ script-users-groups.sh
@@ -0,0 +1,148 @@
+#/bin/bash
+
+
+
+
+
+
+
+echo "Creating folders"
+
+
+
+mkdir /public
+
+
+
+mkdir /adm
+
+
+
+mkdir /ven
+
+
+
+mkdir /sec
+
+
+
+
+
+
+
+echo "Ading users groups"
+
+
+
+groupadd GRP_ADM
+
+
+
+groupadd GRP_VEN
+
+
+
+groupadd GRP_SEC
+
+
+
+
+
+
+
+echo "Adding  user"
+
+
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_ADM
+
+
+
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_ADM
+
+
+
+useradd joao -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_ADM
+
+
+
+
+
+
+
+useradd debora -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_VEN
+
+
+
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_VEN
+
+
+
+useradd roberto -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_VEN
+
+
+
+
+
+
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_SEC
+
+
+
+useradd amanda -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_SEC
+
+
+
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 password) -G GRP_SEC
+
+
+
+
+
+
+
+echo "setting folder's permissions"
+
+
+
+chown root:GRP_ADM /adm
+
+
+
+chown root:GRP_VEN /ven
+
+
+
+chown root:GRP_SEC /sec
+
+
+
+
+
+
+
+chmod 770 /adm
+
+
+
+chmod 770 /ven
+
+
+
+chmod 770 /sec
+
+
+
+chmod 777 /public
+
+
+
+
+
+
+
+echo "All done"
+
+
+
